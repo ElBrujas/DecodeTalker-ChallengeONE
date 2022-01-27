e2c584f1be114a9645f6c8a6194340988b1f04f5
ElBrujas <scamacho@fi.uba.ar>
2022-01-26 21:31:22 -0300

    Agregada la clase Desencriptador

---
+++ script/Desencriptador.js
@@ -0,0 +1,24 @@
+import{Diccionario} from "./Diccionario.js";
+
+class Desencriptador{
+    constructor(){
+        this._miDiccionario = new Diccionario;
+    }
+
+    desencriptarMensaje(unMensaje){
+        let caracteresClave = this._miDiccionario.valores;
+        let mensajeActual = unMensaje;
+
+
+        for (let index = 0; index < caracteresClave.length; index++) {
+            let caracterBuscado = caracteresClave[index];
+            let valorDeCaracter = this._miDiccionario.clavePorValor(caracterBuscado);
+            mensajeActual = mensajeActual.replaceAll(caracterBuscado, valorDeCaracter);
+        }
+
+        return mensajeActual;
+    }
+}
+
+let unEncriptador = new Desencriptador;
+console.log(unEncriptador.desencriptarMensaje("fenterlimescimesdaidenters poberr enternfrenterntair enterstenter dentersaifimesober y haibenterrlober cobernclufatimesdober cobern enterximestober!"));
